e89e84b57f6b6e19102a7b61cab2a99ce941690b
fahisfr <lastorange123@gmail.com>
2021-09-15 20:16:27 +0530

    add product to mongodb (ops (data) not showing fix )just image saving product name

--- helpers/product-helper.js
+++ helpers/product-helper.js
@@ -3,9 +3,11 @@ module.exports = {
 
     addProduct: (product, callback) => {
         console.log(product);
+        console.log("fin")
         db.get().collection('product').insertOne(product).then((data) => {
-            
+            console.log(data)
             callback(data)
+
         })
     }
    
