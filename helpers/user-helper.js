2d8b5d6c2c17cda5f53821749625ed7a6dd34145
fahisfr <lastorange123@gmail.com>
2021-09-27 12:45:55 +0530

    fix cart prducts quantity count (change in user helper (getcartdy and add to cart ) ,cart hbs

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -3,6 +3,7 @@ var collection = require('../config/collections')
 var bcrypt = require('bcrypt')
 const objectid = require('mongodb').ObjectId
 const { response } = require('express')
+const { Collection } = require('mongodb')
 module.exports = {
     doSignup: (userData) => {
         return new Promise(async (resolve, reject) => {
@@ -39,22 +40,42 @@ module.exports = {
             }
         })
     }, addToCart: (proid, userid) => {
+        //create iteam and quantity in {}
+        proobj = {
+            item: objectid(proid),
+            quantity:1
+        }
         return new Promise(async (resolve, reject) => {
+            //checking user cart creat or not
             let usercart = await db.get().collection(collection.CART_COLLECTION).findOne({ user: objectid(userid) })
             if (usercart) {
+                //check prodect in cart 
+                proexist = usercart.products.findIndex(product => product.item == proid)
+                console.log(proexist)
+                //-1 meaning product not cart (array -1 position =null)
+                if (proexist != -1) {
+                    //update product quantity number . finding producs,item == proid then update 
+                    db.get().collection(collection.CART_COLLECTION).updateOne({ 'products.item': objectid(proid) },
+                        {// inc =increase quantity 
+                            $inc:{'products.$.quantity':1}
+                        }
+                    ).then(() => {
+                        resolve()
+                    })
+                } else
+                    //product not in cart .now add new ptoduct and quantity 1 normal 
                 db.get().collection(collection.CART_COLLECTION).updateOne({ user: objectid(userid) },
                     {
-                        $push: { products: objectid(proid) }
-                    }
-                
-                ).then((response) => {
+                        $push: { products: proobj }
+                    }               
+                ).then(() => {
                     resolve()
                 })
-                
+                // else user have no cart create case , create new cart for user 
             } else {
                 let cartobj = {
                     user: objectid(userid),
-                    products: [objectid(proid)]
+                    products: [proobj]
                 }
                 db.get().collection(collection.CART_COLLECTION).insertOne(cartobj).then((response) => {
                     resolve()
@@ -64,29 +85,39 @@ module.exports = {
         })
     },
     getCartProduct: (userId) => {
-        return new Promise(async(resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
+            //(not)aggregate connect all database data
             let cartItems = await db.get().collection(collection.CART_COLLECTION).aggregate([
-                {
+                {//matching cart user id ==userId
                     $match:{user:objectid(userId)}
+                },
+                //before data one {_id ,user=cart owner userid ,products in one[array] }
+                {
+
+                    $unwind: '$products'
+                    //unwind split {id ,user,products:{item :productid,quantity}} (your have cart 2 products , split two {})
+                },
+                {
+                    //take product item and quantity 
+                    $project: {
+                        item: '$products.item',
+                        quantity:'$products.quantity'
+                    }
                 },
                 {
                     $lookup: {
+                        //localField;item (products id), foreignField_ id match products id as products
                         from: collection.PROUCT_COLLECTION,
-                        let: { proList: '$products' },
-                        pipeline: [
-                            {
-                                $match: {
-                                    $expr: {
-                                        $in:['$_id',"$$proList"]
-                                    }
-                                }
-                            }
-                        ],
-                        as:'cartItems'
+                        localField: 'item',
+                        foreignField: '_id',
+                        as:'product'
                     }
                 }
+
             ]).toArray()
-            resolve(cartItems[0].cartItems)
+            console.log(cartItems)
+  
+            resolve(cartItems)
             
         })
     
